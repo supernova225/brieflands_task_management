91859da10df2706c3dc042211291b97513553142
MehDi <49822065+supernova225@users.noreply.github.com>
2023-09-26 10:52:17 +0330

    create LoginController and add its route to api.php

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Http\Request;
+
+/**
+ * @group Auth
+ */
+class LoginController extends Controller
+{
+    /**
+     * Auth Login
+     *
+     * @bodyParam email string required
+     * @bodyParam password string required
+     *
+     */
+    public function login(Request $request)
+    {
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user) {
+            throw new AuthenticationException(['message' => 'کاربری با ایمیل مورد نظر یافت نشد.']);
+        }
+
+        if (!\Hash::check($request->password, $user->password)) {
+            throw new AuthenticationException(['message' => 'رمز عبور اشتباه است.']);
+        }
+
+        $token = $user->createToken('my_task_management')->plainTextToken;
+
+        $response = [
+            'user' => $user,
+            'token' => $token
+        ];
+
+        return response($response);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -14,7 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('register', [\App\Http\Controllers\Auth\RegisterController::class, 'register']);
+// Register
+Route::post('/register', [\App\Http\Controllers\Auth\RegisterController::class, 'register']);
+
+// Login
+Route::post('/login', [\App\Http\Controllers\Auth\LoginController::class, 'login']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
